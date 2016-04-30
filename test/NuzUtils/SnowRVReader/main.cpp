4f1ec80b5bca717e43539c1ed8586f595e4acf8e
星翼 <853974536@qq.com>
2016-04-30 18:53:09 +0800

    Update main.cpp

--- test/NuzUtils/SnowRVReader/main.cpp
+++ test/NuzUtils/SnowRVReader/main.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 int main(){
-    auto lf = Nuz::IEngine::GetGameDevice().GetLocalFile();
-    Nuz::IEngine::GetGameDevice().GetFileSystem().Mount(lf,"testSet");
-    auto p = NuzUtils::ISnowRVReader::CreateSnowRVReader("/testSet/test/NuzUtils/SnowRVReader/test.rv");
-    p -> SaveToFastReadFile("/test/NuzUtils/SnowRVReader/test.elf");
-    p = NuzUtils::ISnowRVReader::CreateSnowRVReader("/testSet/test/NuzUtils/SnowRVReader/test.elf");
+    auto lf = Nuz::IEngine::GetGameDevice().GetLocalFile(); //获取本地文件系统
+    Nuz::IEngine::GetGameDevice().GetFileSystem().Mount(lf,"testSet");  //挂载本地文件系统到文件系统的“testSet”目录下
+    auto p = NuzUtils::ISnowRVReader::CreateSnowRVReader("/testSet/test/NuzUtils/SnowRVReader/test.rv");    //从文件创建SnowRV阅读器
+    p -> SaveToFastReadFile("/test/NuzUtils/SnowRVReader/test.elf");    //保存到二进制码
+    p = NuzUtils::ISnowRVReader::CreateSnowRVReader("/testSet/test/NuzUtils/SnowRVReader/test.elf");    //从二进制码创建SnowRV阅读器
 
-    cout<<p -> GetInt("RVI")<<endl;
-    cout<<p -> GetString("RVS")<<endl;
-    cout<<p -> GetFloat("KEC")<<endl;
-    cout<<p -> GetCurrentDir()<<endl;
+    cout<<p -> GetInt("RVI")<<endl; //获取整型常量RVI
+    cout<<p -> GetString("RVS")<<endl;  //获取字符串常量RVS
+    cout<<p -> GetFloat("KEC")<<endl;   //获取浮点型常量KEC
+    cout<<p -> GetCurrentDir()<<endl;   //获取RV文件所在目录
     return 0;
 }
