eb825f638be810a724dfad54136a0485bca82126
星翼 <853974536@qq.com>
2016-04-30 18:41:37 +0800

    Update main.cpp

--- test/Nuz/FileSystem/FileSystem/main.cpp
+++ test/Nuz/FileSystem/FileSystem/main.cpp
@@ -1,24 +1,24 @@
-#include "../../../../include/Nuz.h"
+#include "../../../../include/Nuz.h"    //Nuz引擎头文件
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
 #include <memory>
 using namespace std;
-using namespace Nuz;
+using namespace Nuz;    //Nuz引擎命名空间
 int main(){
-    IEngine& engine = IEngine::GetGameDevice();
-    IFileSystem& fs = engine.GetFileSystem();
-    auto lf = engine.GetLocalFile();
+    IEngine& engine = IEngine::GetGameDevice(); //取得引擎实例
+    IFileSystem& fs = engine.GetFileSystem();   //取得文件系统
+    auto lf = engine.GetLocalFile();    //取得本地文件系统
 
-    fs.Mount(shared_ptr<IFileSource>(lf));
+    fs.Mount(shared_ptr<IFileSource>(lf));  //把本地文件系统挂载到文件系统的根目录
 
-    auto buf = fs.LoadFile("/test/Nuz/FileSystem/FileSystem/0.txt");
+    auto buf = fs.LoadFile("/test/Nuz/FileSystem/FileSystem/0.txt");    //从文件系统获取文件
 
-    ofstream out("test/Nuz/FileSystem/FileSystem/1.txt",ios::binary);
+    ofstream out("test/Nuz/FileSystem/FileSystem/1.txt",ios::binary);   //写出文件
     out.write((char*)&((*buf)[0]),buf -> size());
     out.close();
 
-    fs.Mount(shared_ptr<IFileSource>(lf));
+    fs.Mount(shared_ptr<IFileSource>(lf));  //再次挂载
 
     return 0;
 }
