cdc7bb6da30b8e128c462c7a992b653b2cdef8c9
星翼 <853974536@qq.com>
2016-05-04 20:56:46 +0800

    添加ACNReader

---
+++ include/NuzUtils/ACNReader.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <cstdint>
+
+namespace NuzUtils{
+
+	/* ACN树形配置文件阅读器
+	 */
+	class IACNReader{
+	public:
+        /* 树创建指导指令 */
+        /* 树根由阅读器指导创建。 */
+        /* 可能会有多个树根，这由你的配置文件决定。 */
+        /* 请编写安全的代码来接受阅读器指导创建树。 */
+        enum class ACNOpr{
+            PushAndCreateNode = 0x00,  //将当前节点指针压栈并创建一个新的节点，参数为节点名
+            Pop = 0x01,    //设置栈顶的节点指针为当前节点指针，并弹出栈顶，参数为空
+            CreateVar = 0x10,  //为当前节点创建一个属性，参数为属性名，并设置为当前属性
+            AddVal = 0x11,  //为当前属性添加一个值
+            End = 0xFF //树创建结束
+        };
+
+        /* 取得一个指导指令
+         * @param 指令参数
+         * @result 指令
+         */
+        virtual ACNOpr GetOpr(std::string& arg) = 0;
+
+        /* 从头重新读取指导指令
+         */
+        virtual void Reset() = 0;
+	};
+}
+
+/* 更新日志：
+ * 2016年5月4日：
+ * 星翼 初稿
+ */
